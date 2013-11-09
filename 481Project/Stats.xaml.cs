af05e18bef284c9e5cfdc12990e1699dc0847db6
Parker <parkertfish@gmail.com>
2013-11-09 12:42:19 -0700

    Just Testing out Git really. Started on Roster and Stats Widgets.

---
+++ 481Project/Stats.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _481Project
+{
+    /// <summary>
+    /// Interaction logic for Stats.xaml
+    /// </summary>
+    public partial class Stats : UserControl
+    {
+        public Stats()
+        {
+            InitializeComponent();
+        }
+
+        private void add_Click(object sender, RoutedEventArgs e)
+        {
+            string c = this.stackPanel.Children.Count.ToString();                 //Gets number of elements from stackPanel
+            TextBlock b = new TextBlock();                                              //TODO: Make acutal player elements 
+            b.Text = "Fake Stats " + c;
+            this.stackPanel.Children.Add(b);
+
+        }
+
+        private void remove_Click(object sender, RoutedEventArgs e)
+        {
+            int c = this.stackPanel.Children.Count;
+            if (c > 0) { this.stackPanel.Children.RemoveAt(c - 1); }                //Removes last added elements
+
+        }
+    }
+}
