0322ab0b8a375d32c32380d5d7866ab4db586764
FourLeafClover <ireland.alex@yahoo.ca>
2013-11-17 19:42:12 -0700

    Started work on custom calendar object.
    Basic layout for calendar as well as preparation for button click event
    set up.

---
+++ 481Project/CustomCalendar.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _481Project
+{
+    /// <summary>
+    /// Interaction logic for CustomCalendar.xaml
+    /// </summary>
+    public partial class CustomCalendar : UserControl
+    {
+        public CustomCalendar()
+        {
+            InitializeComponent();
+            weekDay1.Day.Content = "Sunday";
+            weekDay2.Day.Content = "Monday";
+            weekDay3.Day.Content = "Tuesday";
+            weekDay4.Day.Content = "Wednesday";
+            weekDay5.Day.Content = "Thursday";
+            weekDay6.Day.Content = "Friday";
+            weekDay7.Day.Content = "Saturday";
+        }
+    }
+}
---
+++ 481Project/Date.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _481Project
+{
+    /// <summary>
+    /// Interaction logic for Date.xaml
+    /// </summary>
+    public partial class Date : UserControl
+    {
+        public Date()
+        {
+            InitializeComponent();
+        }
+
+        private void border1_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            //Use this to open up our modal window.
+        }
+
+    }
+}
